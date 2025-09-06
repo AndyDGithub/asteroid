c6bf972549538954e0e308b46d3cf3a49275225f
ThePrimeagen <the.primeagen@aol.com>
2025-09-06 18:54:13 +0200

    finish base game

--- main.py
+++ main.py
@@ -1,21 +1,57 @@
 import pygame
 from constants import *
+from circleshape import CircleShape
+from player import Player, Shot
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
 	pygame.init()
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	clock = pygame.time.Clock()
+
+	updatable = pygame.sprite.Group()
+	drawable = pygame.sprite.Group()
+	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
+
+	Asteroid.containers = (asteroids, updatable, drawable)
+	Shot.containers = (shots, updatable, drawable)
+	AsteroidField.containers = updatable
+	asteroid_field = AsteroidField()
+
+	Player.containers = (updatable, drawable)
+
+	player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_WIDTH / 2)
+
+	dt = 0
 
 	while True:
 		for event in pygame.event.get():
-		    if event.type == pygame.QUIT:
-		        return
+			if event.type == pygame.QUIT:
+				return
+		
+		updatable.update(dt)
+
+		for asteroid in asteroids:
+			if asteroid.is_colliding(player):
+				print("Game over!")
+				return
+			for shot in shots:
+				if shot.is_colliding(asteroid):
+					shot.kill()
+					asteroid.split()
+
+
 		screen.fill((0, 0, 0))
 
+		for obj in drawable:
+			obj.draw(screen)
+
 		pygame.display.flip()
-	#print("Starting Asteroids!")
-	#print(f"Screen width: {SCREEN_WIDTH}")
-	#print(f"Screen height: {SCREEN_HEIGHT}")
+
+		dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
-    	main()
+	main()
